948624d28fd657a2c5bffd61148808d58f0632b9
Kenneth Sevelsted <kensted@gmail.com>
2018-09-07 12:37:00 +0200

    Tilføjede table viewer og arbede på anden skærm

--- Emoji app/ViewController.swift
+++ Emoji app/ViewController.swift
@@ -8,11 +8,29 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
 
+    var emoji = ["🤵🏻","😱","👌🏻","🔔","👏🏻","😆","💰"]
+    
+    @IBOutlet weak var primetableviewr: UITableView!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        primetableviewr.dataSource = self
+        primetableviewr.delegate = self
+    
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return emoji.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        print(indexPath.row)
+        let cell = UITableViewCell()
+        cell.textLabel?.text = emoji[indexPath.row]
+        return cell
     }
 
     override func didReceiveMemoryWarning() {
